e22ecaa7d5cf62a649bf85b5ebc91c2576b215c4
jalho <alho.jere@gmail.com>
2025-03-22 15:24:39 +0200

    Customize prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,18 @@
 # > fish --version
 # fish, version 4.0.1
 
+function fish_prompt
+    set -l last_status $status
+    # Prompt status only if it's not 0
+    set -l stat
+    if test $last_status -ne 0
+        set stat (set_color red)"[$last_status]"(set_color normal)
+    end
+
+    echo -e (string join '' -- (set_color green) (prompt_pwd) (set_color normal) $stat)
+    echo -n '$ '
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
